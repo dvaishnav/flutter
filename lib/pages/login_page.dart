e0e97a06e16ecfc1d8772f1614e58fcffe37aac8
Deepak vaishnav <deepak.v@cognustechnology.com>
2023-04-01 14:37:08 +0530

    theme

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,61 +10,84 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = '';
+
+  final _FormKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_FormKey.currentState!.validate()) {
+      Navigator.pushNamed(context, AppRouts.homeRoute);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(children: [
-          Image.asset(
-            "images/login.png",
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Welcome $name",
-            style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    label: Text("Username"),
-                    hintText: "Enter username",
-                  ),
-                  onChanged: ((value) {
-                    name = value;
-                    setState(() {});
-                  }),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    label: Text("Password"),
-                    hintText: "Enter password",
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, AppRouts.homeRoute);
-                  },
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
-                )
-              ],
+        child: Form(
+          key: _FormKey,
+          child: Column(children: [
+            Image.asset(
+              "images/login.png",
+              fit: BoxFit.cover,
             ),
-          )
-        ]),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome $name",
+              style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text("Username"),
+                      hintText: "Enter username",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter username";
+                      }
+                      return null;
+                    },
+                    onChanged: ((value) {
+                      name = value;
+                      setState(() {});
+                    }),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      label: Text("Password"),
+                      hintText: "Enter password",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter username";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => moveToHome(context),
+                    child: Text("Login"),
+                    style:
+                        TextButton.styleFrom(minimumSize: const Size(150, 40)),
+                  )
+                ],
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
